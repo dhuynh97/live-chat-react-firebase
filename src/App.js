0604f6fc833a9b340f77ff3d92a0aabac7fce710
Daniel Huynh <danielhuynh97@gmail.com>
2021-02-09 16:21:32 -0800

    Linked Firebase config files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ import 'firebase/firestore';
 import { useAuthState } 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDV8EIxZT83Bi_jtCDXEc9MOpBZWvfwrQw",
+  authDomain: "live-chat-1ba24.firebaseapp.com",
+  projectId: "live-chat-1ba24",
+  storageBucket: "live-chat-1ba24.appspot.com",
+  messagingSenderId: "118764391609",
+  appId: "1:118764391609:web:675939bb3f4f4806b99f68",
+  measurementId: "G-J5KNPBT0XP"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 function App() {
   return (
     <div className="App">
