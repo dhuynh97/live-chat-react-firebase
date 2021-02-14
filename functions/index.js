dbb5193ad10ef5d1b03750f250cdcc7d7953ccec
Daniel Huynh <danielhuynh97@gmail.com>
2021-02-13 18:39:51 -0800

    Changed Firebase rule functions

--- functions/index.js
+++ functions/index.js
@@ -16,9 +16,8 @@ exports.detectEvilUsers = functions.firestore
         if (filter.isProfane(text)) {
 
             const cleaned = filter.clean(text);
-            await doc.ref.update({text: `🤐 I got BANNED for life for saying... ${cleaned}`});
+            await doc.ref.update({text: `${cleaned}`});
 
-            await db.collection('banned').doc(uid).set({});
         } 
 
         const userRef = db.collection('users').doc(uid)
